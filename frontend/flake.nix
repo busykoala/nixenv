ebf08437fd98424507bfdd06da17f92fe823007b
Matthias Osswald <matthias@busykoala.io>
2025-05-08 09:49:38 +0200

    Add frontend env.

---
+++ frontend/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Template flake for direnv shells";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }: let
+    additionalPackages = pkgs: [
+      pkgs.nodejs_20
+      pkgs.pnpm_10
+    ];
+  in
+  flake-utils.lib.eachDefaultSystem (system:
+  let
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.default = pkgs.mkShell {
+      buildInputs = additionalPackages pkgs;
+
+      shellHook = ''
+        echo "🧪  Welcome to your dev shell!"
+        echo "🔧  Tools included: ${toString (map (p: "🔹 ${pkgs.lib.getName p}") (additionalPackages pkgs))}"
+      '';
+    };
+  });
+}
+
